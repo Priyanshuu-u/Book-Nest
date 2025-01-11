41f19508199d645357118fdd510d5e0149bacedd
Priyanshu Rajpurohit <officialpriyanshu14@gmail.com>
2025-01-11 21:15:32 +0530

    Update Signup.jsx

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -17,7 +17,7 @@ function Signup() {
       email:data.email,
       password:data.password
     }
-   await axios.post(`{url}/user/signup`,userInfo)
+   await axios.post(`${url}/user/signup`,userInfo)
     .then((res)=>{
       console.log(res.data);
       if(res.data){
